285b59cd8877fbe47fad2da5797c17eb3b983b2e
Dmitry <65062995+Dimkabit@users.noreply.github.com>
2021-10-05 22:42:16 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 let iconMenu = document.querySelector(".icon-menu");
 let body = document.querySelector("body");
 let menuBody = document.querySelector(".menu__body");
+
 if (iconMenu) {
 	iconMenu.addEventListener("click", function () {
 		iconMenu.classList.toggle("active");
 		menuBody.classList.toggle("active");
-		body.classList.toggle("lock");
+		
 		
 	});
 }
