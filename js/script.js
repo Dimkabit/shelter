941060d981a711f2b2ca7cfa9e07600bc0b94cc4
Dmitry <65062995+Dimkabit@users.noreply.github.com>
2021-10-07 22:23:45 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
 let iconMenu = document.querySelector(".icon-menu");
 let body = document.querySelector("body");
 let menuBody = document.querySelector(".menu__body");
-const activeLink = document.querySelectorAll('.menu__link');
 
-//active link cheng
-const deactive = () => {
-	activeLink.forEach(btn => btn.classList.remove('active-link'))
-}
-
-activeLink.forEach((btn, i) => {
-	btn.addEventListener('click', () => {
-		if(!btn.classList.contains('active-link')) {
-			deactive();
-			btn.classList.add('active-link');
-		}
-	})
-})
-
-
-//menu burger
 if (iconMenu) {
 	iconMenu.addEventListener("click", function () {
 		iconMenu.classList.toggle("active");
 		menuBody.classList.toggle("active");
+		
+		
 	});
 }
